91b18c5bf256f726cec6a995578320b226d8d34e
xu <695540791@qq.com>
2014-08-12 11:45:03 +0800

    grunt调整

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,13 @@ module.exports = function (grunt){
         pkg:grunt.file.readJSON('package.json'),
         concat:{
             options:{
-                separator:';'
+                separator:'\n'
             },
             dist:{
                 src:[
                     'myCanvasDrawing/src/js/init.js',
-                    'myCanvasDrawing/src/js/baseDrawing.js',
-                    'myCanvasDrawing/src/js/moduleRelativeToFun.js',
-                    'myCanvasDrawing/src/js/help.js',
-                    'myCanvasDrawing/src/js/mainDrawingFuns.js',
-                    'myCanvasDrawing/src/js/Polygon.js',
-                    'myCanvasDrawing/src/js/relateToRubber.js',
-                    'myCanvasDrawing/src/js/event.js'
+                    'myCanvasDrawing/src/js/*.js',
+                    '!myCanvasDrawing/src/js/drawing.js'
                 ],
                 dest:'myCanvasDrawing/dist/<%= pkg.name%>.js'
             }
@@ -33,16 +28,37 @@ module.exports = function (grunt){
             files:['test/**/*.html']
         },
         jshint:{
-            files:['Gruntfile.js','myCanvasDrawing/src/js/*.js','test/**/*.js'],
             options:{
-                jQuery:true,
-                console:true,
-                module:true
+                curly:true,
+                eqeqeq:true,
+                eqnull:true,
+                browser:true
+            },
+            all:['Gruntfile.js','myCanvasDrawing/src/js/*.js','myCanvasDrawing/dist/<%= pkg.name%>.js','test/**/*.js'],
+            beforeconcat:['Gruntfile.js','myCanvasDrawing/src/js/*.js','test/**/*.js'],
+            afterconcat:{
+                options:{
+                    curly:true,
+                    eqeqeq:true,
+                    eqnull:true,
+                    browser:true,
+                    unused:true,
+                    undef:true
+                },
+                files:{
+                    src:['myCanvasDrawing/dist/myCanvasDrawing.js']
+                }
             }
+
         },
         watch:{
             files:['<%= jshint.files%>'],
-            tasks:['jshint','qunit']
+            tasks:['jshint:all','concat','uglify']
+        },
+        exec:{
+            restart_app:{
+                cmd:"echo 'my name is xu'"
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-concat');
@@ -50,7 +66,8 @@ module.exports = function (grunt){
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-exec');
 
     grunt.registerTask('test',['jshint']);
-    grunt.registerTask('default',['jshint','concat','uglify']);
+    grunt.registerTask('default',['jshint:all','concat','uglify']);
 };
