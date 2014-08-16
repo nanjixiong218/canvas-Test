0bb199bfea2d44fae4764cb86a67fc66551338d1
xu <695540791@qq.com>
2014-08-16 21:18:35 +0800

    移动成功，代码结构不好，还要考虑好边界问题

--- example/js/together.js
+++ example/js/together.js
@@ -10,21 +10,75 @@ var Together = function(stretch ,timeLine){
 
 };
 Together.prototype = {
-    animate : function (StretchBegin,StretchEnd,timeLineBegin,timeLineEnd,text){
-        var radius = StretchBegin;
-        var x = timeLineBegin;
-        var alp = 0;
-        var sWidth = StretchEnd - StretchBegin;
-        var tWidth = timeLineEnd - timeLineBegin;
+    animate : function (StretchBegin,StretchEnd,sDir,timeLineBegin,timeLineEnd,tDir,text,aDir){
+        var radius = sDir==1?StretchBegin:StretchEnd;
+        var x = tDir==1?timeLineBegin:timeLineEnd;
+        var alp = aDir==1?0:1;
+        var sWidth = Math.abs(StretchEnd - StretchBegin);
+        var tWidth = Math.abs(timeLineEnd - timeLineBegin);
         var that = this;
+        var dirStr = ""+sDir+tDir+aDir;
 
         var loop = function (){
-            x=x+tWidth/60;
-            radius = radius + sWidth/60;
-            alp = alp+1/60;
 
+            radius = (sDir==1?radius + sWidth/60:radius-sWidth/60);
+            if(radius>=StretchEnd||radius<=StretchBegin){
+                radius = sDir==1?StretchEnd:StretchBegin;
+            }
+            x = (tDir==1?x+tWidth/60:x-tWidth/60);
+            if(x>=timeLineEnd||x<=timeLineBegin){
+                x = tDir==1?timeLineEnd:timeLineBegin;
+            }
+            alp = (aDir==1?alp+1/60:alp-1/60);
+            if(alp>=1||alp<=0){
+                alp = aDir==1?1:0;
+            }
             that.stretch.fillCanvas('charcoal');
-            if(radius<=StretchEnd||x<=timeLineEnd){
+
+
+            var ifDo =  false;
+            var path = [];
+            path.push({
+                dir:"111",
+                bool:radius<StretchEnd||x<timeLineEnd||alp>0
+            });
+            path.push({
+                dir:"110",
+                bool:radius<StretchEnd||x<timeLineEnd||alp<1
+            });
+            path.push({
+                dir:"101",
+                bool:radius<StretchEnd||x>timeLineBegin||alp>0
+            });
+            path.push({
+                dir:"100",
+                bool:radius<StretchEnd||x>timeLineBegin||alp<1
+            });
+            path.push({
+                dir:"011",
+                bool:radius>StretchBegin||x<timeLineEnd||alp>0
+            });
+            path.push({
+                dir:"010",
+                bool:radius>StretchBegin||x<timeLineEnd||alp<1
+            });
+            path.push({
+                dir:"001",
+                bool:radius>StretchBegin||x>timeLineBegin||alp>0
+            });
+            path.push({
+                dir:"000",
+                bool:radius>StretchBegin||x>timeLineBegin||alp<1
+            });
+
+            path.forEach(function(obj){
+                if(obj.dir ===dirStr){
+                    ifDo = obj.bool;
+                }
+                //forEach中如何终止循环，return肯定不行TODO，用break或者continue可以么？
+            });
+
+            if(ifDo){
                 that.context.save();
                 that.stretch.drawAnimationFrame(radius);
                 that.context.restore();
@@ -48,9 +102,9 @@ Together.prototype = {
     },
     drawText :function(text,alp){
         this.context.save();
-        this.context.font = '128px red';//必须设置字体,非法的也可以
-        this.context.fillStyle = 'rgba(0,200,0,'+alp+')';
-        this.context.fillText(text,200,200);
+        this.context.font = '16px red';//必须设置字体,非法的也可以
+        this.context.fillStyle = 'rgba(0,0,150,'+alp+')';
+        this.context.fillText(text,30,50);
         this.context.restore();
     }
 };
